bfacff5d189711732b76e0f1f8862e3bff32eac0
Balazs Gollner <Balazs_Gollner@epam.com>
2014-06-04 01:40:16 +0200

    no message

--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,6 @@ var routes = require('../routes');
 
 module.exports = function (app) {
 
+    app.get('/', routes.activitylog.activityReport);
     app.post('/report', routes.rest.registerRegionChange);
-    app.get('/activityLog', routes.activitylog.activityReport);
 }
